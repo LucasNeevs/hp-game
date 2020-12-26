01376394fb3438f1c1f44eb12da2813cffbf2454
LucasNeevs <lucasneves.dev@gmail.com>
2020-12-25 22:01:11 -0300

    Updates

---
+++ precache-manifest.df073bef32782bc189f1451e17a58498.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421a2d8ad24578df8c43a0f7629571fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ba9e5a88e01bbc8e24",
+    "url": "/static/js/2.e43b2737.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.e43b2737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fce8207e1cd67facfe3",
+    "url": "/static/js/main.d51d7d9d.chunk.js"
+  },
+  {
+    "revision": "6aa967c5e7da02fc7a82",
+    "url": "/static/js/runtime-main.2ff89fd5.js"
+  },
+  {
+    "revision": "61b7c23bc3d58e4c4b7776285606a2b6",
+    "url": "/static/media/hog-bg.61b7c23b.webp"
+  },
+  {
+    "revision": "767b51a5fb4895312f9073b33fa0cc79",
+    "url": "/static/media/hp-title.767b51a5.png"
+  }
+]);
